ccde3aab801431c0e727e0fec108aa3abc4fab9b
AYeshsidd <159470548+AYeshsidd@users.noreply.github.com>
2024-04-05 16:24:30 -0700

    console and edit a name

---
+++ index.ts
@@ -0,0 +1,60 @@
+import inquirer from "inquirer";
+
+let myBalance = 50000;
+
+let pin = 7070;
+
+let pincode = await inquirer.prompt([
+  { name: "ayesh",
+     message: "Enter your PIN", 
+     type: "number" } 
+   
+    ]);
+if (pincode.ayesh === pin) {console.log("Account sucessfully login");
+
+  
+    let operator = await inquirer.prompt([
+      {name: "action",
+      message: "please select one of operator",
+      type: "list",
+      choices: ["checking Balance", "withdraw", "deposit"] }
+    
+  ]);
+
+  if (operator.action === "withdraw") {
+    let amountW = await inquirer.prompt([
+      { name: "withdrawnAmount",
+        message: "Enter the withdrawn amount",
+        type: "number"}
+     ]);
+if (amountW.withdrawnAmount < myBalance) {
+      myBalance -= amountW.withdrawnAmount;
+      
+      console.log("your reamining balance is" + " " + myBalance);}
+    
+      else {console.log("insufficnt Fudns");}
+
+    } 
+  
+  
+  else if (operator.action === "checking Balance") 
+  {console.log(myBalance);}
+   
+  
+  else if (operator.action === "deposit") {
+    let amountD = await inquirer.prompt([
+      { name: "depositAmount",
+        message: "amount to deposit is ?",
+        type: "number" }
+    
+    ]);
+
+    myBalance += amountD.depositAmount;
+
+    console.log("now your balance is" + " " + myBalance);
+ 
+} // Last else-if closed here 
+
+} // very First if closed here
+
+    else {console.log("incorrect pin code");}
